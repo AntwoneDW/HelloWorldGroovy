59974ae9bea2dd4375a9d956d33542bd9ee98f0a
Antwone Walters <antwone@gmail.com>
2020-04-21 20:48:25 -0400

    Initial commit

---
+++ src/Example.groovy
@@ -0,0 +1,7 @@
+class Example {
+    static void main(String[] args) {
+        /* This program is the first program
+        This program shows how to display hello world */
+        println('Hello World');
+    }
+}
